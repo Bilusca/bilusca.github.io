08605326f130f4a5b4797d9796d629c6909a35cd
Lucas Vieira <lucasav2005@hotmail.com>
2018-11-26 15:34:11 -0200

    Vai danado

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,3 +5,12 @@
  */
 
 // You can delete this file if you're not using it
+exports.onCreateBabelConfig = ({ stage, actions }, pluginOptions) => {
+    const ssr = stage === `build-html` || stage === `build-javascript`
+  
+    actions.setBabelPlugin({
+      name: `babel-plugin-styled-components`,
+      stage,
+      options: { ...pluginOptions, ssr },
+    })
+  }
--- src/components/ui/BigCard.js
+++ src/components/ui/BigCard.js
@@ -1,4 +1,15 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
+
+const fadeUp = keyframes`
+    0% {
+        opacity: 0;
+        transform: translateY(100px);
+    }
+    100% {
+        opacity: 1;
+        transform: translateY(0);
+    }
+`
 
 export const BigCard = styled.div`
     box-sizing: border-box;
@@ -7,6 +18,7 @@ export const BigCard = styled.div`
     background-color: #fff;
     border-radius: 2px;
     box-shadow: 0 1px 2px 0 rgba(0, 0, 0, 0.2);
+    animation: ${fadeUp} 1s;
     @media (max-width: 480px) {
         width: 80%;
     }
